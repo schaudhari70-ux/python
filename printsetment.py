ce5b637bc1cbc8958bad2e1a316ef27f89178eee
schaudhari70-ux <schaudhari70@gmail.com>
2026-06-15 16:45:33 +0530

    Add files via upload

---
+++ printsetment.py
@@ -0,0 +1,10 @@
+print("hello world")
+def devarsh_ ():
+    print("my name is devarsh")
+def soccer_ ():
+    print("my favroite sport is football")
+def number_ ():
+    print("the anwser of 3+5is",3+5)
+devarsh_()
+soccer_()
+number_()
