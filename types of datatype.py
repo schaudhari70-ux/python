9c4fcfb2f9aa2d954052d71fcfc1c7883bffb8a1
schaudhari70-ux <schaudhari70@gmail.com>
2026-07-03 18:43:17 +0530

    Add files via upload

---
+++ types of datatype.py
@@ -0,0 +1,8 @@
+name="devarsh"
+print("type of name:",type(name))
+naumber=3
+print("type of naumber:",type(naumber))
+decimal=3.5
+print('type of decimal:',type(decimal))
+tryry=True
+print("type of tryry:",type(tryry))
