e144f933df043e0d6958e3f4bb1d85c772839422
schaudhari70-ux <schaudhari70@gmail.com>
2026-07-03 18:57:44 +0530

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,17 @@
+name="pegiun"
+age=23
+weigth =12.8
+isstudent=True
+print("name:",name)
+print("type of nameis:",type(name))
+print("age:",age)
+print("type of ageis:",type(age)) 
+print("weight:",weigth)
+print("type of wegthis:",type(weigth))  
+print("isstudent:",isstudent)
+print("type ofisstudent:",type(isstudent))  
+#after type casting
+name=int(name)
+print(type(name))
+age=str(12)
+print(type(age))
