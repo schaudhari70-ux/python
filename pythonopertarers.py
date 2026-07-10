9413795c8271012fe8252be41522cffa3c84bd85
schaudhari70-ux <schaudhari70@gmail.com>
2026-07-10 19:00:00 +0530

    Add files via upload

---
+++ pythonopertarers.py
@@ -0,0 +1,20 @@
+field1=120
+field2=34
+field3=56
+field4=140
+field5=78
+total=field1+field2+field3+field4+field5
+average=total/5
+print("Total:", total, "kg")
+print("the Average:", average, "kg")
+price=15
+earings=total*price
+print("Total Earnings:", earings, "rupees")
+print("price=", price, "rupees")
+bags=total//25
+leftover=total%25
+print("Total Bags:", bags,"is there are twentyfive kg per bag theleftover is",leftover,"kg")
+lastyear=500
+print("same as last year",lastyear==total)
+print("better then last years",total>lastyear)
+print("good as",total>=lastyear)
