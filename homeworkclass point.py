c7ac1b20c1cc3d1f650b0243006be7e67d1cd444
schaudhari70-ux <schaudhari70@gmail.com>
2026-07-10 19:27:58 +0530

    Add files via upload

---
+++ homeworkclass point.py
@@ -0,0 +1,13 @@
+class1=120
+class2=30
+clas3=140
+class4=56
+class5=78
+total=class1+class2+clas3+class4+class5
+average=total/5 
+print("Total:", total, "points")
+print("Average:", average, "points")
+lastyear=300
+print("same as last year",lastyear==total)
+print("better then last years",total>lastyear)
+print("good as",total>=lastyear)
