8237e7bf3c6aabd0a7ddd402f35be63572372df9
schaudhari70-ux <schaudhari70@gmail.com>
2026-07-19 12:47:49 +0530

    Add files via upload

---
+++ wether outfitpicker.py
@@ -0,0 +1,28 @@
+anwser=int(input("what is the temperture in celsis"))
+if anwser <20:
+    print("wear a jacket as it is cold")
+else:
+    print("wear tshirt")
+anwser=(input("is it raining"))
+if  anwser=="yes":
+    print("bring a umbralla")
+else:print("donot do bring anthing for rain")
+anwser=int(input("what is the wind speed in km/h"))
+if anwser>30:
+    windbreaker="yes"
+    print("it is windy ")
+    print("wear your tshirt")
+else:
+    print("it is not windy")
+    print("wear a outfit")
+anwser=input("is it having puddles?")
+if anwser=="yes":
+    print("do not go outside")
+else:
+    print("you can go outside")
+    
+
+
+
+
+
