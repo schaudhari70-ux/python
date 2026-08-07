f075f4b65a82045c6a52ba0048ecd775edbbcc41
schaudhari70-ux <schaudhari70@gmail.com>
2026-08-07 18:47:18 +0530

    Add files via upload

---
+++ is not is .py
@@ -0,0 +1,21 @@
+x=34 
+if (type(x)is int):
+    print("true")
+else:
+    print("false")
+x=5.5
+if (type(x)is float):
+    print("ANSWER IS TRUE")
+else:
+    print("ANSWER IS FALSE")
+x=20
+y=20
+if (x is y):
+     print("x and y are same identy")
+else:
+    print("x is different")
+y=439
+if (x is not y):
+ print("y is different")
+else:
+    print("y is same")
